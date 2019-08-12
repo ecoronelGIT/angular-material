21c867fd5b4806071a307835a9fc0a15ec49feab
Ezequiel Coronel <ezequiel.coronel@despegar.com>
2019-08-12 19:10:21 -0300

    Initial commit in angular 6, angular material

---
+++ product-cart/server.js
@@ -0,0 +1,17 @@
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/product-cart'));
+
+app.get('/*', function(req,res) {
+    console.log('__dirname:'+__dirname);
+res.sendFile(path.join(__dirname+'/dist/product-cart/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8084);
+console.log("PORT 8084")
